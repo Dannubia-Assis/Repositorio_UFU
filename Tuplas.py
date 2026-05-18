7e1e78d629344882bce3916f3f4ddb19b03d67a3
Dannubia-Assis <dannubia77@gmail.com>
2026-05-18 19:51:44 -0300

    Atualização aula de tupla

--- Tuplas.py
+++ Tuplas.py
@@ -30,7 +30,7 @@ print(tupla4)
 #Gerando tupla com range
 tupla = tuple(range(11))
 print(tupla)
-"""
+
 #Desempacotamento de tuplas
 
 tupla = ('Universidade', 'Uberlândia')
@@ -41,5 +41,34 @@ print(cidade)
 
 #Gera ValueError se colocarmos um número diferente de elementos para desempacotar
 
+#Métodos para adição e remoção de tuplas não existem
 
+#Pode somar tuplas mas não é possível modificar elas
+
+#São imutáveis mas é possível sobrescrever seus valores, colocar
+#mas na outra
+
+#Para saber se um elemento está em uma tupla: retorna True ou False
+
+tupla = (1, 2, 3)
+
+print(3 in tupla)
+
+for n in tupla:
+    print(n)
+
+for indice, valor in enumerate(tupla):
+    print(indice, valor)
+    """
+
+#Contas elementos dentro de uma tupla
+
+tupla = ('a', 'b', 'c', 'd', 'e', 'a','b')
+
+print(tupla.count('a'))
+
+#Dicas de utilizadade de tuplas
+
+#Devemos utilizar tuplas sempre que não precisamos modificar
+# os dados contidos em uma coleção
 
