43b42ad7cc042ac90822ae935c88d02705685011
Dannubia-Assis <dannubia77@gmail.com>
2026-05-18 22:05:08 -0300

    Atualização de Dicionário

--- dicionarios.py
+++ dicionarios.py
@@ -3,5 +3,102 @@ Dicionários
 - Em algumas linguagens são conhecidos por mapas
 - São coleções do tipo chave e valor
 - São representados por chaves {}
+- Chave e valor são separados por dois pontos 'chave:valor'
+- Chave e valor podem ser de qualquer tipo
+- Dados podem ser misturados
+
+#Criação de dicionários
+
+#Forma 1 (mais comum)
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+print(paises)
+print(type(paises))
+
+#Forma 2 (menos comum)
+
+paises = dict(br='Brasil', eua='Estados Unidos', py='Paraguay')
+
+print(paises)
+print(type(paises))
+
+#Acessando elementos
+
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+#Forma 1 - Acessando via chave da mesma forma que lista/tupla
+print(paises['br'])
+
+#Forma 2 - Acessando via get - Recomendado pois não gera erro e retorna o tipo None
+
+print(paises.get('br'))
+print(paises.get('ru')) # O tipo none em python representa o tipo sem tipo (vazio)
+#Primeira letra sem maiuscula None, sempre é considerado como false
+
+#Podemos definir um valor padrão para caso não encontremos o objeto com
+#a chave informada  
+
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+pais = paises.get('ru', 'Não encontrado') #Se não encontrar mostra o segundo argumento
+
+print(f'Encontrei o pais {pais}')
+
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+#Podemos verificar se determinada chave se encontra em um dicionário
+
+print('br'in paises)
+print('ru' in paises)
+print('Estados Unidos' in paises)
+
+#Pode ser utilizado qualquer tipo de dado
+#Tuplas são interessantes para serem utilizadas em dicionarios por serem imutáveis
+
+localidade = { 
+    (32.58, 38.59): 'Escritório em Tókio',
+    (96.58, 15.59): 'Escritório em São Paulo'
+    }
+
+print(localidade)
+print(type(localidade))
+
+
+#Adicionar elementos em um dicionário
+
+receita = {'jan':100, 'fev': 120, 'mar':300}
+
+print(receita)
+print(type(receita))
+
+#Forma 1 - Mais comum
+
+receita['abr'] = 350
+
+print(receita)
+
+#Forma 2 
+receita = {'jan':100, 'fev': 120, 'mar':300}
+
+mes = input("Digite o mês: ")
+valor = int(input("Digite o valor: "))
+
+#Colchetes [] servem para acessar uma chave, adicionar uma chave e atualizar uma chave
+#Chaves {} criam o dicionário
+#Também dá para usar o .update()
+
+receita[mes]=valor
+
+print(receita)
+
+#A forma de atualizar ou adicionar novos elementos em um dicionário
+#é a mesma
+# Não pode existir chaves repetidas em dicionários
 
 """
+#Remover dados de um dicionário
+
+
+
+
+
