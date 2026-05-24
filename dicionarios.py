59b2f4c2b8f89d6a638740af8a2bd38a8514a12d
Dannubia-Assis <dannubia77@gmail.com>
2026-05-24 20:36:56 -0300

    Atualizacao

--- dicionarios.py
+++ dicionarios.py
@@ -169,7 +169,33 @@ print(usuario)
 # Ele vai gerar para cada valor do interável uma chave
 # e irá atribuir a esta chave o valor informado
 
+#Acessando valores 
+
+print(receita.values())
+
+for valor in receita.values():
+    print(valor)
+
+#Desempacotamento de dicionários
+
+print(receita.items())
+
+for chave, valor in receita.items():
+    print(f'chave={chave} e valor={valor}')
 """
+
+receita = {'jan':100, 'fev':250, 'mar':400}
+
+print(receita)
+
+#Soma*, valor máximo*, valor mínimo*, tamanho
+# * Se os valores forem todos inteiros ou reais
+print(receita.items())
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita))
+
 
 
 
